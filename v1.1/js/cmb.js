e9f585a39eb08717eb4d251a69c623ab0689c472
Leviosar <joaocampo2102@gmail.com>
2018-04-19 20:48:33 -0300

    Inserção de dados

---
+++ v1.1/js/cmb.js
@@ -0,0 +1,19 @@
+var results = document.querySelector('div.results')
+
+var servForm = document.querySelector('form#servform')
+servForm.inputs = {} = document.querySelectorAll('div.input-group')
+servForm.addons = {} = document.querySelectorAll('span.input-group-addon')
+
+servForm.addons[0].addEventListener('click', ev =>{
+    window.location = 'http://labmatii.online/QuadroDocente/camboriulist.php'
+})
+
+function callAjax(){
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', 'camboriu.php')
+    
+    xhr.addEventListener('load', ev =>{
+        results.innerText += xhr.responseText;
+    })
+    xhr.send();
+}
