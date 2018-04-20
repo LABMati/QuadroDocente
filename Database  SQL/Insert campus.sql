85a65ecb355b326c779760b6bdfbb11b00c5308a
JoaoIA16IFC <jvr_090701@hotmail.com>
2018-04-19 21:03:54 -0300

    Agora vai
    Arrumando um erro no Insert professor

---
+++ Database  SQL/Insert campus.sql
@@ -0,0 +1,16 @@
+INSERT INTO campus(cidade_campus) VALUES
+('Abelardo Luz'),
+('Araquari'),
+('Blumenau'),
+('Brusque'),
+/*('Camboriú'),*/
+('Concórdia'),
+('Fraiburgo'),
+('Ibirama'),
+('Luzerna'),
+('Rio do Sul'),
+('Santa Rosa do Sul'),
+('São Bento do Sul'),
+('São Francisco do Sul'),
+('Sombrio'),
+('Videira');
