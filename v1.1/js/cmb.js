e65f4609d4c2773b7a216b5735797d68c56db8f2
felipekriffel <34461134+felipekriffel@users.noreply.github.com>
2018-04-19 22:28:19 -0300

    Update cmb.js

--- v1.1/js/cmb.js
+++ v1.1/js/cmb.js
@@ -1,19 +1,14 @@
-var results = document.querySelector('div.results')
-
-var servForm = document.querySelector('form#servform')
-servForm.inputs = {} = document.querySelectorAll('div.input-group')
-servForm.addons = {} = document.querySelectorAll('span.input-group-addon')
-
-servForm.addons[0].addEventListener('click', ev =>{
-    window.location = 'http://labmatii.online/QuadroDocente/camboriulist.php'
-})
-
-function callAjax(){
+var responses
+var search = "Marcelo" //document.querySelector("div.search > input")
+function showResult(search){
+    if(search.length==0){
+        return
+    }
     var xhr = new XMLHttpRequest()
-    xhr.open('GET', 'camboriu.php')
-    
-    xhr.addEventListener('load', ev =>{
-        results.innerText += xhr.responseText;
+    xhr.open('GET', 'http://127.0.0.1:8080/search.php?q='+search, true)
+    xhr.addEventListener('load', ev=>{
+        responses = JSON.parse(xhr.responseText)
+        console.log(responses)
     })
-    xhr.send();
+    xhr.send()
 }
