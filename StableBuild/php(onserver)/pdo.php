e482e2a17fcf280a4f5c37cf40d392801adf7c46
Leviosar <joaocampo2@hotmail.com>
2018-09-28 23:52:44 -0300

    Just exclusions

--- StableBuild/php(onserver)/pdo.php
+++
@@ -1,4 +0,0 @@
-<?php
-    return new PDO("mysql:host=mysql.hostinger.com.br;dbname=u535468846_quad", "u535468846_lab", "labmatii", [
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
-    ]);
